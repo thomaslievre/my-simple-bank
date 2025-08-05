bb0d8c50acbc3d3511fed581e3131d649069b4f3
Thomas Lièvre <thomaslievre.pro@protonmail.com>
2025-08-05 23:09:40 +0200

    Update user.go

--- internal/api/user.go
+++ internal/api/user.go
@@ -3,6 +3,7 @@ package api
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgerrcode"
@@ -12,10 +13,18 @@ import (
 )
 
 type createUserRequest struct {
-	Username string `json:"username" binding:"required, alphanum"`
-	Password string `json:"password" binding:"required, min=6"`
-	Email    string `json:"email" binding:"required, email"`
-	FullName string `json:"fullname" binding:"required"`
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	Email    string `json:"email" binding:"required,email"`
+	FullName string `json:"full_name" binding:"required"`
+}
+
+type userResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
@@ -55,5 +64,13 @@ func (server *Server) createUser(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, user)
+	resp := userResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt.Time,
+		CreatedAt:         user.CreatedAt.Time,
+	}
+
+	ctx.JSON(http.StatusOK, resp)
 }
